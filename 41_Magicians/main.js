ab457d7b9db2a3e59bd94fc249740492ce66ac92
Nimra-Iqbal-Qureshi <nimishahidiq26@gmail.com>
2024-07-19 14:56:16 -0700

    first commit

---
+++ 41_Magicians/main.js
@@ -0,0 +1,7 @@
+var magicians = ["Harry Houdini", "David Copperfield", "Uri Geller", "David Blaine"];
+function show_magician(magician) {
+    for (var i = 0; i < magician.length; i++) {
+        console.log(magicians[i]);
+    }
+}
+show_magician(magicians);
---
+++ 41_Magicians/main.ts
@@ -0,0 +1,12 @@
+let magicians:string[]=["Harry Houdini","David Copperfield","Uri Geller","David Blaine"];
+
+function show_magician(magician:string[]) {
+for (let i = 0; i < magician.length; i++) {
+console.log(magicians[i]);
+
+    
+}
+        
+    }
+
+    show_magician(magicians)
