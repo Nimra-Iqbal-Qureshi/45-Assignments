82f6715d659693f6baab5ac5edf35ce319b48bbd
Nimra-Iqbal-Qureshi <nimishahidiq26@gmail.com>
2024-07-19 14:49:51 -0700

    first commit

---
+++ 38_Cities/main.ts
@@ -0,0 +1,8 @@
+function describe_city(city:string,country:string="Pakistan") {
+    
+    console.log(`${city} is in ${country}.`);
+    
+}
+describe_city("Hyderabad")
+describe_city("Khairpur")
+describe_city("Dhaka","Bangladesh")
