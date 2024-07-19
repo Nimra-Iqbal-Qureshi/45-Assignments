f2449b29204a1b76d633d6162869e5505502cf19
Nimra-Iqbal-Qureshi <nimishahidiq26@gmail.com>
2024-07-19 13:41:41 -0700

    first commit

---
+++ 13_Own_Array/main.js
@@ -0,0 +1,5 @@
+var favtransportation = ["Train", "Auto Rikshaw", "Bike", "Car"];
+console.log("I enjoy travelling by", favtransportation[0]);
+console.log("I enjoy travelling by", favtransportation[1]);
+console.log("I enjoy travelling by", favtransportation[2]);
+console.log("I enjoy travelling by", favtransportation[3]);
---
+++ 13_Own_Array/main.ts
@@ -0,0 +1,6 @@
+let favtransportation:string[]=["Train","Auto Rikshaw","Bike","Car"];
+
+console.log("I enjoy travelling by" ,favtransportation[0]);
+console.log("I enjoy travelling by" ,favtransportation[1]);
+console.log("I enjoy travelling by" ,favtransportation[2]);
+console.log("I enjoy travelling by" ,favtransportation[3]);
