5bb926a87f8dbffea702b71ccb2ee8bdad6c34b3
Nimra-Iqbal-Qureshi <nimishahidiq26@gmail.com>
2024-07-19 13:43:23 -0700

    first commit

---
+++ 14_Guest_List/main.js
@@ -0,0 +1,4 @@
+var guestList = ["Asad", "Shahid", "Zahid", "Salar"];
+for (var i = 0; i < guestList.length; i++) {
+    console.log("Dear " + guestList[i] + ",\nIt is our pleasure to invite you on a dinner.\nThank You");
+}
---
+++ 14_Guest_List/main.ts
@@ -0,0 +1,5 @@
+let guestList:string[]=["Asad","Shahid","Zahid","Salar"];
+for (let i = 0; i < guestList.length; i++) {
+    console.log("Dear " + guestList[i] + ",\nIt is our pleasure to invite you on a dinner.\nThank You");
+    
+}
