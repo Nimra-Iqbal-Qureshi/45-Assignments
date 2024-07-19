98e3eac65560c733a87add6fe1b41eae28de9ff5
Nimra-Iqbal-Qureshi <nimishahidiq26@gmail.com>
2024-07-19 14:09:04 -0700

    first commit

---
+++ 23_Conditional_Test/main.js
@@ -0,0 +1,31 @@
+var car = 'subaru';
+//test # 1
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+//test # 2
+console.log("Is car === 'subaru'? I predict True.");
+console.log(car === 'subaru');
+//test # 3
+console.log("Is car != 'civic'? I predict True.");
+console.log(car != 'civic');
+//test # 4
+console.log("Is car !== 'corola'? I predict True.");
+console.log(car !== 'corola');
+//test # 5
+console.log("Is car.upper case == 'SUBARU'? I predict True.");
+console.log(car.toUpperCase() == 'SUBARU');
+// test # 6
+console.log("Is car.upper case == 'subaru'? I predict False.");
+console.log(car.toUpperCase() == 'subaru');
+//test # 7
+console.log("Is car.lower case == 'SUBARU'? I predict False.");
+console.log(car.toLowerCase() == 'SUBARU');
+//test # 8
+console.log("Is car == 'SUBARU'? I predict False.");
+console.log(car == 'SUBARU');
+//test # 9
+console.log("Is car == 'subaru'? I predict False.");
+console.log(car == 'subaru ');
+//test # 10
+console.log("Is car == 'Honda'? I predict False.");
+console.log(car == 'Honda');
