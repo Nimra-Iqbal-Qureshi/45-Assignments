b82afdcecad07567bc69470ba229627342fef98b
Nimra-Iqbal-Qureshi <nimishahidiq26@gmail.com>
2024-07-19 14:14:00 -0700

    first commit

---
+++ 25_Alien_Color_#_1/main.js
@@ -0,0 +1,6 @@
+var alien_color = "green";
+if (alien_color === "green") {
+    console.log("The player just earn 5 points");
+}
+alien_color = "red";
+if (alien_color === "green") { }
---
+++ 25_Alien_Color_#_1/main.ts
@@ -0,0 +1,9 @@
+let alien_color:string="green";
+
+if (alien_color==="green") {
+    console.log("The player just earn 5 points");
+}
+
+alien_color="red";
+    if (alien_color==="green") {}
+    
