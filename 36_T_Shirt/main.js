d1e08abcd27897e1eb0aa1252e02664b29b5c7fe
Nimra-Iqbal-Qureshi <nimishahidiq26@gmail.com>
2024-07-19 14:47:10 -0700

    first commit

---
+++ 36_T_Shirt/main.js
@@ -0,0 +1,6 @@
+function makeShirt(size, text) {
+    console.log("Creating a ".concat(size, " T-Shirt with the message: ").concat(text));
+}
+makeShirt("small", "Have a nice day.");
+makeShirt("madium", "Have a nice day.");
+makeShirt("large", "Have a nice day.");
---
+++ 36_T_Shirt/main.ts
@@ -0,0 +1,6 @@
+function makeShirt(size:string,text:string) {
+    console.log(`Creating a ${size} T-Shirt with the message: ${text}`);
+}
+makeShirt("small","Have a nice day.");
+makeShirt("madium","Have a nice day.");
+makeShirt("large","Have a nice day.");
