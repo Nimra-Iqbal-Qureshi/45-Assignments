2e23a9bf43422748b78d8c2b92446cf942994d73
Nimra-Iqbal-Qureshi <nimishahidiq26@gmail.com>
2024-07-19 14:44:36 -0700

    first commit

---
+++ 34_Pizzas/main.ts
@@ -0,0 +1,6 @@
+let pizzas:string[]=["BBQ chicken","Cheeze","Pepperoni"];
+
+for(let i=0;i<pizzas.length;i++){
+    console.log(`I like ${pizzas[i]} pizza.`);
+}
+console.log(`I really love pizza!`);
