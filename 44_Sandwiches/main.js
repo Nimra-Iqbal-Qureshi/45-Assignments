7dd5e4e1b6db3844a09a8c441119489ea2d8dc99
Nimra-Iqbal-Qureshi <nimishahidiq26@gmail.com>
2024-07-19 15:00:23 -0700

    first commit

---
+++ 44_Sandwiches/main.js
@@ -0,0 +1,8 @@
+function makeSandwich(item) {
+    console.log("\nMaking Sandwich with these items:\n");
+    console.log("".concat(item));
+    console.log("\nEnjoy your Sandwich!");
+}
+makeSandwich(["Chicken Kabab", "Cheese"]);
+makeSandwich(["Egg", "Cheese", "Lettuce"]);
+makeSandwich(["Lettuce", "Tomato", "Mayo", "Onion", "Chicken"]);
---
+++ 44_Sandwiches/main.ts
@@ -0,0 +1,13 @@
+function makeSandwich(item:string[]) {
+    console.log("\nMaking Sandwich with these items:\n");
+    
+    
+console.log(`${item}`);
+
+    console.log("\nEnjoy your Sandwich!");
+    
+    
+}
+makeSandwich(["Chicken Kabab","Cheese"]);
+makeSandwich(["Egg","Cheese","Lettuce"]);
+makeSandwich(["Lettuce","Tomato","Mayo","Onion","Chicken"]);
