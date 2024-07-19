d4e64b3ccdb1aee3a7a80d76a4a47eb1f5f87e58
Nimra-Iqbal-Qureshi <nimishahidiq26@gmail.com>
2024-07-19 13:36:13 -0700

    first commit

---
+++ 10_Adding_Comments/main.js
@@ -0,0 +1,4 @@
+//Making a varible with keyword let and datatype string.
+var myName = "Nimra Iqbal Qureshi";
+//Print my name through console
+console.log(myName);
---
+++ 10_Adding_Comments/main.ts
@@ -0,0 +1,8 @@
+//Making a varible with keyword let and datatype string.
+let myName:string="Nimra Iqbal Qureshi";
+
+//Print my name through console
+console.log(myName);
+
+
+
