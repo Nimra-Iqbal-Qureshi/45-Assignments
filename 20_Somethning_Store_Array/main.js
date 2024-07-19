157aa3592937d0a6c867d21cf71b6900cb67d919
Nimra-Iqbal-Qureshi <nimishahidiq26@gmail.com>
2024-07-19 13:57:21 -0700

    first commit

---
+++ 20_Somethning_Store_Array/main.js
@@ -0,0 +1,8 @@
+var cities = ["Karachi", "Hyderabad", "Khairpur", "Multan"];
+console.log(cities);
+var items = [];
+items.push("Pencil");
+items.push("Eraser");
+items.push("Sharpner");
+items.push("Scale");
+console.log(items);
---
+++ 20_Somethning_Store_Array/main.ts
@@ -0,0 +1,12 @@
+let cities:string[]=["Karachi","Hyderabad","Khairpur","Multan"]
+console.log(cities);
+
+let items:string[]=[];
+items.push("Pencil")
+items.push("Eraser")
+items.push("Sharpner")
+items.push("Scale")
+
+console.log(items);
+
+
