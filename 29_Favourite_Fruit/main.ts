bee4e75d7561284ae859457fc908cf6b5d0437e3
Nimra-Iqbal-Qureshi <nimishahidiq26@gmail.com>
2024-07-19 14:32:30 -0700

    first commit

---
+++ 29_Favourite_Fruit/main.ts
@@ -0,0 +1,17 @@
+let favouriteFruit:string[]=["Mango","Grapes","Watermelon"];
+
+if (favouriteFruit.includes("Mango")) {
+    console.log("I really like Mango!");  
+} 
+if (favouriteFruit.includes("Apple")) {
+    console.log("I really like Apple!");  
+} 
+if (favouriteFruit.includes("Kiwi")) {
+    console.log("I really like Kiwi!");  
+} 
+if (favouriteFruit.includes("Grapes")) {
+    console.log("I really like Grapes!");  
+} 
+if (favouriteFruit.includes("Watermelon")) {
+    console.log("I really like Watermelon!");  
+} 
