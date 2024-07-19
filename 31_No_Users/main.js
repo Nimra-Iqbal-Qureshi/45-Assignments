f3d7b35682b9d831642399a5726bf7723be75ff0
Nimra-Iqbal-Qureshi <nimishahidiq26@gmail.com>
2024-07-19 14:37:43 -0700

    first commit

---
+++ 31_No_Users/main.js
@@ -0,0 +1,6 @@
+var userName = ["Admin", "Zahid", "Shahid", "Asad", "Ahmed"];
+userName.splice(0, 5);
+// console.log(userName);
+if (userName.length === 0) {
+    console.log("We need to find some users!");
+}
---
+++ 31_No_Users/main.ts
@@ -0,0 +1,13 @@
+let userName:string[]=["Admin","Zahid","Shahid","Asad","Ahmed"];
+userName.splice(0,5)
+
+
+
+if (userName.length===0) {
+    console.log("We need to find some users!");
+}
+
+
+
+
+
