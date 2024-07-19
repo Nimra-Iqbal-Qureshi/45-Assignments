af9e52281caee203a47e5ec289ebcdd78c3fbf5e
Nimra-Iqbal-Qureshi <nimishahidiq26@gmail.com>
2024-07-19 14:15:15 -0700

    first commit

---
+++ 26_Alien_Color_#_2/main.ts
@@ -0,0 +1,16 @@
+let alien_color:string="green";
+
+//Version 1 of this program that runs if block
+if (alien_color==="green") {
+    console.log("The player just earn 5 points for shooting the Alien.");
+}else{
+    console.log("The player just earned 10 points.");
+    
+}
+//Version 2 of this program that runs else block
+
+if (alien_color==="yellow") {
+    console.log("The player just earn 5 points ofr shooting the Alien.");
+}else{
+    console.log("The player just earn 10 points.")
+}
