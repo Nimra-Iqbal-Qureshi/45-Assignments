2ca0c960dc8556141075bae799a7294757f8917b
Nimra-Iqbal-Qureshi <nimishahidiq26@gmail.com>
2024-07-19 13:39:58 -0700

    first commit

---
+++ 12_Greetings/main.js
@@ -0,0 +1,6 @@
+var names = ["Shahid", "Asad", "Zahid", "Salar"];
+var message = ", would you like to eat some apples ?";
+console.log(names[0], message);
+console.log(names[1], message);
+console.log(names[2], message);
+console.log(names[3], message);
---
+++ 12_Greetings/main.ts
@@ -0,0 +1,6 @@
+let names:string[]=["Shahid","Asad","Zahid","Salar"];
+let message:string=", would you like to eat some apples ?"
+console.log(names[0] , message);
+console.log(names[1] , message);
+console.log(names[2] , message);
+console.log(names[3] , message);
