46ede44d0085719bf0ed175db9a350d3ed7560b1
Nimra-Iqbal-Qureshi <nimishahidiq26@gmail.com>
2024-07-19 14:01:24 -0700

    first commit

---
+++ 21_Typescript_Object/main.js
@@ -0,0 +1,18 @@
+//method # 1
+var person = {
+    name: "Asad",
+    age: 20,
+    student: true,
+    day: "Thursday",
+    timimg: "9 to 12"
+};
+console.log(person);
+//Method # 2
+var person1 = { name: 'Asad', age: 20, student: true };
+console.log(person1);
+var obj = {
+    name: "kala",
+    age: 4,
+    height: "2inch"
+};
+console.log(obj);
---
+++ 21_Typescript_Object/main.ts
@@ -0,0 +1,29 @@
+//method # 1
+
+interface Person{
+    name:string,
+    age:number,
+    student:boolean,
+    day:string,
+    timimg:string
+
+}
+
+let person:Person={
+    name:"Asad",
+    age:20,
+    student:true,
+    day:"Thursday",
+    timimg:"9 to 12"
+
+}
+console.log(person);
+
+//Method # 2
+
+let person1:{name:string,age:number,student:boolean}={name:'Asad',age:20,student:true}
+console.log(person1);
+
+
+
+
