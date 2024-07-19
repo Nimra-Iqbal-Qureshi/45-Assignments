d3d5a980193e39d911a29a16f6dfac4e2cd98f44
Nimra-Iqbal-Qureshi <nimishahidiq26@gmail.com>
2024-07-19 14:45:55 -0700

    first commit

---
+++ 35_Animals/main.js
@@ -0,0 +1,5 @@
+var animals = ["Parrot", "Dog", "Cat"];
+for (var i = 0; i < animals.length; i++) {
+    console.log("A ".concat(animals[i], " would make a great pet."));
+}
+console.log("Any of these animals would make a great pet!");
