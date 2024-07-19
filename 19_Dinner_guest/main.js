92d078056749d041eb38f1e59e0da88a458f9186
Nimra-Iqbal-Qureshi <nimishahidiq26@gmail.com>
2024-07-19 13:53:53 -0700

    first commit

---
+++ 19_Dinner_guest/main.js
@@ -0,0 +1,5 @@
+var guestList = ["Asad", "Shahid", "Zahid", "Salar"];
+// for (let i = 0; i < guestList.length; i++) {
+// console.log("Dear " + guestList[i] + ",\nIt is our pleasure to invite you on a dinner.\nThank You");
+console.log("I am inviting total ".concat(guestList.length, " guests for dinner."));
+//}
