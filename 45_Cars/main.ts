4bf83609439d96c4ffa2c8860663e94d6d9b554d
Nimra-Iqbal-Qureshi <nimishahidiq26@gmail.com>
2024-07-19 15:11:00 -0700

    first commit

--- 45_Cars/main.ts
+++ 45_Cars/main.ts
@@ -1,18 +1,16 @@
 
-
 function storecarInfo(manufacture:string,modelNumber:string,...extraOption:{[key:string]:any}[]){
-let carInfo={
-    manufacture,
-    modelNumber,
-    ...Object.assign({},...extraOption)
-}
-return carInfo
-}
-let ans1=storecarInfo("Toyota","2024")
-console.log(ans1);
-
-
-let ans=storecarInfo("Honda","2023",{color:"Black"},{features:["navigation","power window"]})
-console.log(ans);
-
-
+    let carInfo={
+        manufacture,
+        modelNumber,
+        ...Object.assign({},...extraOption)
+    }
+    return carInfo
+    }
+    let ans1=storecarInfo("Toyota","2024")
+    console.log(ans1);
+    
+    
+    let ans=storecarInfo("Honda","2023",{color:"Black"},{features:["navigation","power window"]})
+    console.log(ans);
+    
